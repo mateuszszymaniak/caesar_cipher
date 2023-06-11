03ad96e1c4b3a6b4c291807329263a2b804d9e74
Mateusz Szymaniak <maszipl@gmail.com>
2023-06-11 20:48:29 +0200

    feat(savetojson): add feature to add crypted text to json file

--- src/main.py
+++ src/main.py
@@ -1,3 +1,52 @@
+import os
+
+from src.FileHandler import FileHandler
+
+def chose_rot_type() -> str:
+    encrypt_options = ['ROT13', 'ROT47']
+    print("\nDostępne rodzaje szyfrowań")
+    for value, key in enumerate(encrypt_options, start=1):
+        print(f"{value}. {key}")
+    encrypt_option_chosen = int(input("Wybierz rodzaj szyfrowania: "))
+    if encrypt_option_chosen - 1 < len(encrypt_options):
+        return encrypt_options[encrypt_option_chosen - 1]
+    else:
+        print("Podano nieprawidłową wartość!")
+        chose_rot_type()
+
+def get_file_name() -> tuple[str, (str, None)]:
+    did_add_to_file = None
+    file_name = input("Podaj nazwę pliku: ") + '.json'
+    if os.path.isfile(file_name):
+        did_add_to_file = input("Podana nazwa pliku istnieje. Czy chcesz dodać zawartość do pliku? [t/n]: ")
+        if did_add_to_file == 't':
+            did_add_to_file = 't'
+        elif did_add_to_file == 'n':
+            get_file_name()
+        else:
+            print("Podano nieprawidłową akcję!")
+            get_file_name()
+    return file_name, did_add_to_file
+
+def save_to_file_question(memory_buffer: list[str], rot_tyoe: str, status: str):
+    answer = input("Czy zapisać do pliku? [t/n]: ").lower()
+    if isinstance(answer, str):
+        if answer == 't':
+            file_name, did_override = get_file_name()
+            if did_override == 't':
+                memory_buffer.insert(0, FileHandler.append(file_name))
+            for text in memory_buffer:
+                file = FileHandler(text, rot_tyoe, status)
+                file.save(file_name, did_override)
+        elif answer == 'n':
+            pass
+        else:
+            print("Podano nieprawidłowy znak")
+            save_to_file_question(memory_buffer)
+    else:
+        print("Podano nieprawidłowy znak")
+        save_to_file_question(memory_buffer)
+
 def rot13(txt: str) -> str:
     result = ""
     for letter in txt:
@@ -30,20 +79,13 @@ def encrypt(memory_buffer: list[str], encrypt_option: str) -> list[str]:
             encrypt_result.append(rot47(text))
     return encrypt_result
 
-def to_decode(memory_buffer: list[str]) -> list[str]:
-    encrypt_options = ['ROT13', 'ROT47']
+def str_to_decode(memory_buffer: list[str]) -> list[str]:
     print("\nPusty napis kończy dodawanie napisów do szyfrowania")
     while True:
         txt = input("Podaj tekst do zaszyfrowania: ")
         if txt == '':
             break
         memory_buffer.append(txt)
-    print("\nDostępne rodzaje szyfrowań")
-    for value, key in enumerate(encrypt_options, start=1):
-        print(f"{value}. {key}")
-    encrypt_option_chosen = int(input("Wybierz rodzaj szyfrowania: "))
-    memory_buffer = encrypt(memory_buffer, encrypt_options[encrypt_option_chosen - 1])
-
     return memory_buffer
 
 def main():
@@ -55,11 +97,12 @@ def main():
         option = input("Wybierz opcję: ")
         match option:
             case "1":
-                to_decode(memory_buffer)
-                print("Szyfruję")
+                str_to_decode(memory_buffer)
+                rot_type = chose_rot_type()
+                memory_buffer = encrypt(memory_buffer, rot_type)
+                save_to_file_question(memory_buffer, rot_type, "encrypted")
             case "2":
-                to_decode(memory_buffer)
-                print("Odszyfruję")
+                str_to_decode(memory_buffer)
             case "3":
                 print("Zamykam program")
                 break
