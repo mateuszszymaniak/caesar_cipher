0e13e1a86f9704c15d37fc223858cae07d7e0c53
Mateusz Szymaniak <maszipl@gmail.com>
2023-06-13 20:58:31 +0200

    refactor(text): move text to separate class

--- src/FileHandler.py
+++ src/FileHandler.py
@@ -1,42 +1,13 @@
 import json
-import os
-from dataclasses import dataclass, asdict
 
-@dataclass
 class FileHandler:
-    text: str
-    rot_type: str
-    status: str
-
-    @property
-    def get_text(self):
-        return self.text
-
-    @get_text.setter
-    def get_text(self, value):
-        self.text = value
-
-    @property
-    def get_rot_type(self):
-        return self.rot_type
-
-    @get_rot_type.setter
-    def get_rot_type(self, value):
-        self.rot_type = value
-
-    @property
-    def get_status(self):
-        return self.status
-
-    @get_status.setter
-    def get_status(self, value):
-        self.status = value
-
-    def save(self, file_name: str, did_override: (str, None)):
+    @staticmethod
+    def save(file_name: str, did_override: (str, None)):
         with open(file_name, 'w') as writer:
-            json.dump(asdict(self), writer)
+            json.dump(asdict(self), writer)# to fix
 
-    def open(self, file_name):
+    @staticmethod
+    def open(file_name):
         with open(file_name, 'r') as reader:
             json.load(reader)
 
---
+++ src/Text.py
@@ -0,0 +1,5 @@
+class Text:
+    def __init__(self, txt: str, rot_type: str, status: str):
+        self.txt = txt
+        self.rot_type = rot_type
+        self.status = status
