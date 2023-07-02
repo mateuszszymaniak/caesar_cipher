1dfc95009dee0b72585d8799acb3979cf7c4abd1
Mateusz Szymaniak <maszipl@gmail.com>
2023-07-02 20:47:22 +0200

    refactor: change method for displaying available rots

--- src/cipher_type.py
+++ src/cipher_type.py
@@ -1,16 +1,16 @@
 from enum import Enum
 
+from src.rots import Rots
+
 
 class CipherType(Enum):
-    ROT13 = "ROT13"
-    ROT47 = "ROT47"
-
     @staticmethod
     def show_all() -> None:
         """
-        Method display all available rot types
+        Method display all available rot types from rots class
 
         :return: None
         """
-        for key, value in enumerate(CipherType, start=1):
-            print(f"{key}. {value.value}")
+        available_ciphers = [a for a in dir(Rots) if "rot" in a]
+        for key, value in enumerate(available_ciphers, start=1):
+            print(f"{key}. {value.upper()}")
