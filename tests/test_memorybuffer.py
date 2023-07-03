8638558f77558c16f276cb72624ec9f8e9a1bc37
Mateusz Szymaniak <maszipl@gmail.com>
2023-07-03 17:54:12 +0200

    test: added next tests

--- tests/test_memorybuffer.py
+++ tests/test_memorybuffer.py
@@ -9,13 +9,22 @@ def set_mock_memorybuffer(mocker):
     my_mock.memory_buffer = [Text("azx", "rot_13", "encrypted")]
     return my_mock
 
+
 def test_add(set_mock_memorybuffer):
     new_obj = Text("qwe", "rot_47", "to_encrypt")
     set_mock_memorybuffer.add_to_memory_buffer(new_obj)
     assert set_mock_memorybuffer.get_length() == 2
 
+
 def test_is_empty(set_mock_memorybuffer):
     assert set_mock_memorybuffer.is_empty
 
+
 def test_clear_memory_buffer(set_mock_memorybuffer):
     assert set_mock_memorybuffer.clear_memory_buffer
+    set_mock_memorybuffer.clear_memory_buffer()
+
+
+def test_is_empty_without_mock():
+    memorybuffer = MemoryBuffer
+    assert memorybuffer.is_empty()
